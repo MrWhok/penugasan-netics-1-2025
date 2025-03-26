5ce2f40afaeda5155ab589902417373a11bd6a2a
MrWhok <rafifaydin.a@gmail.com>
2025-03-26 15:14:56 +0700

    initial code

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const moment = require('moment');
+const startTime=Date.now();
+
+app.get('/health', (req, res) => {
+    const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+    const uptime = (Date.now() - startTime) / 1000;
+    res.json({
+        nama: "Tunas Bimatara Chrisnanta Budiman",
+        nrp: "5025231999",
+        status: "UP",
+        timestamp: currentTime,
+        uptime: uptime
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 8080');
+});
