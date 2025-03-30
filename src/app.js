4f3115c4748896259f3cc78131a96ed4f62894e7
MrWhok <rafifaydin.a@gmail.com>
2025-03-30 12:52:48 +0700

    Fix status

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.get('/health', (req, res) => {
     res.json({
         nama: "Tunas Bimatara Chrisnanta Budiman",
         nrp: "5025231999",
-        status: "DOWN",
+        status: "UP",
         timestamp: currentTime,
         uptime: uptime
     });
